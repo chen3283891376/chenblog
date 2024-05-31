76be6b6340733a3350f54556efd636a9da60c704
chen3283891376 <3283891376@qq.com>
2024-05-31 19:08:10 +0800

    Add files via upload

---
+++ to_HTML.py
@@ -0,0 +1,33 @@
+import markdown
+import bs4
+import os
+ 
+def markdown_to_html(text):
+    return markdown.markdown(text)
+
+file_list = os.listdir("./my_blog")
+print(file_list)
+for i in file_list:
+    aaa = i.split(".")
+    # 示例Markdown文本
+    markdown_file = open("./my_blog/"+i,"r",encoding="utf-8")
+    markdown_text = markdown_file.read()
+    markdown_file.close()
+
+    # 转换Markdown到HTML
+    html_text = markdown_to_html(markdown_text)
+    file = open(aaa[0]+".html","w",encoding="utf-8")
+    file.write(html_text)
+    file.close()
+
+    with open('index.html','r',encoding="utf-8") as f:
+        text = f.read()
+    soup = bs4.BeautifulSoup(text,'html.parser')
+    new_a = soup.new_tag("a",href="./"+aaa[0]+".html",align="center")
+    new_a.string = aaa[0]
+    br = soup.new_tag("br")
+    soup.body.div.append(new_a)
+    soup.body.div.append(br)
+    new_html = soup.prettify()
+    with open('index.html','w',encoding="utf-8") as f:
+        f.write(new_html)
