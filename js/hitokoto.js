344e3d70bfee1a3e7eb0e6855d81c2b9a9a1c081
chen3283891376 <3283891376@qq.com>
2025-05-11 11:12:31 +0800

    fix: 处理hitokoto加载失败时显示的文本

--- js/hitokoto.js
+++ js/hitokoto.js
@@ -1,7 +1,11 @@
 const hitokoto = async () => {
+    let hitokoto = "加载中...";
     const response = await fetch('https://v1.hitokoto.cn/?c=k&charset=utf-8');
+    if (!response.ok) {
+        return "谨以此献给学而思最好的那段时光, 以及我们失去的一切。 —— 凌";
+    }
     const data = await response.json();
-    const hitokoto = `${data.hitokoto} —— ${data.from_who}「${data.from}」`;
+    hitokoto = `${data.hitokoto} —— ${data.from_who}「${data.from}」`;
     return hitokoto;
 }
 (function() {
